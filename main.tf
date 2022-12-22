abc8b0c5be99c5113519dd0a262498c2f6d62151
Mykyta-Zhuravlov <120449688+Mykyta-Zhuravlov@users.noreply.github.com>
2022-12-22 12:15:33 +0200

    v1.1.2

--- main.tf
+++ main.tf
@@ -15,3 +15,59 @@ resource "random_password" "password" {
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+resource "aws_ssm_parameter" "db_password" {
+  name  = "db_password"
+  type  = "String"
+  value = random_password.password.result
+}
+
+data "aws_ami" "aws_ami_ecs" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["591542846629"]
+}
+
+
+
+data "aws_iam_policy_document" "ecs_tasks_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
+  role       = aws_iam_role.test_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core_role" {
+  role       = aws_iam_role.test_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
